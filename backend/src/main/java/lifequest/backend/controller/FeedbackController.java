0416f97608b02a9acd27c0088b8220e879a4face
asmondnoahloo <157396430+asmondnoahloo@users.noreply.github.com>
2024-05-14 00:08:32 +0800

    new push of react client and admin frontend

--- backend/src/main/java/lifequest/backend/controller/FeedbackController.java
+++ backend/src/main/java/lifequest/backend/controller/FeedbackController.java
@@ -8,6 +8,7 @@ import lifequest.backend.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,7 @@ public class FeedbackController {
         return new ResponseEntity<>(feedbackService.getFeedbackById(id), HttpStatus.OK);
     }
 
-    @PostMapping("/feedback/getAll")
+    @GetMapping("/feedback/getAll")
     public ResponseEntity<Iterable<Feedback>> getAllFeedback() {
         return new ResponseEntity<>(feedbackService.getAllFeedback(), HttpStatus.OK);
     }
---
+++ client-frontend/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomepageHeader from '../components/homepageHeader';
+import HomepageCarousel from '../components/homepageCarousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Homepage = () => {
+    return (
+       <>
+         <HomepageHeader />
+         <br />
+          <HomepageCarousel />
+       </>
+    );
+};
+
+export default Homepage;
