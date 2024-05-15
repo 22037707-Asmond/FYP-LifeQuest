57cefd45758f55e55de62521f3cacabd83eaf587
Avril <leongavril@gmail.com>
2024-05-15 13:06:04 +0800

    Altered Sign in with Header & Created AboutUs Page

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import SignIn from './pages/SignIn';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>} />
       <Route path='SignIn' element = {<SignIn/>} ></Route>s
+      <Route path='AboutUs' element = {<AboutUs/>} ></Route>s
+
     </Routes>
   );
 }
