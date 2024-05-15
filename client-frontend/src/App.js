f87fa9a0b91e1f378d1b9fc9d43a079d5ba832a5
Avril <leongavril@gmail.com>
2024-05-15 13:28:42 +0800

    Changed Aboutus Page

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import SignIn from './pages/SignIn';
-import AboutUs from './pages/AboutUs';
+import AboutUs from './pages/AboutUS';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>} />
       <Route path='SignIn' element = {<SignIn/>} ></Route>s
-      <Route path='AboutUs' element = {<AboutUs/>} ></Route>s
+      <Route path='AboutU' element = {<AboutUs/>} ></Route>s
 
     </Routes>
   );
---
+++ client-frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AboutUS() {
+  return (
+  
+       <div>
+            <h1>About Us</h1>
+            <p>Welcome to our website! We are a team of passionate individuals dedicated to providing the best user experience for our customers.</p>
+            <p>Feel free to explore our website and learn more about our mission, values, and the services we offer.</p>
+        </div>
+   
+  )
+}
+
+export default AboutUS;
+
