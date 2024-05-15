5b44b1f5b6a223e1eb904726f02d8a1b22d16e7c
asmondnoahloo <157396430+asmondnoahloo@users.noreply.github.com>
2024-05-15 11:25:17 +0800

    added Sign In Page to client frontend

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>} />
+      <Route path='SignIn' element = {<SignIn/>} ></Route>s
     </Routes>
   );
 }
