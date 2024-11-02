744a78e7e2d920b61badce503486d246efacd331
asmondnoahloo <157396430+asmondnoahloo@users.noreply.github.com>
2024-11-02 12:09:32 +0800

    add pip requirements to middleware folder

--- Flask Middleware/app/__init__.py
+++ Flask Middleware/app/__init__.py
@@ -9,20 +9,18 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     
-    # MySQL database configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SuperPowerMelon3!@127.0.0.1:3306/lifequest'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'your_secret_key_here'
     
     CORS(app, resources={r"/*": {"origins": "*"}})
     
-    # Initialize the database with the app
     db.init_app(app)
     
-    # Initialize Flask-Migrate with the app and database
+
     migrate.init_app(app, db)
 
-    # Import and register the Blueprint
+   
     from app.routes.predict import predict_bp
     app.register_blueprint(predict_bp)
 
